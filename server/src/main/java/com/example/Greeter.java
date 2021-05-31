e43cf11014b8a08aa7d7d6eaff643be8058b897f
chriskag <85107500+chriskag@users.noreply.github.com>
2021-05-31 16:24:20 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,9 +1,6 @@
 package com.example;
 
-/** 
-*@HelloClass 
-*
-*/
+
 public int checkReturnTag(final int aTagIndex,
                           JavadocTag[] aTags,
                           int aLineNo)
